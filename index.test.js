a07d60d01449862454833b2ab8abc8cf555f6224
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-25 17:30:18 -0700

    delete all the created files after the test is done

--- index.test.js
+++ index.test.js
@@ -4,19 +4,28 @@ const randomWord = require('./random-word');
 
 describe('rename a large group of files according to a certain syntax', () => {
   beforeEach(done => {
-    for(let i = 0; i < 100; i++) {
-      const path = `./${i}.txt`;
+    for(let i = 1; i <= 100; i++) {
+      const path = `./test-files/${i}.txt`;
       const data = randomWord();
       fs.writeFile(path, data, done);
     }
   });
+
+
   afterEach(done => {
     fs.readdir('./test-files', (err, files) => {
       if(err) console.error(err);
 
       for(const file of files) {
-        fs.unlink(path.join('/test-files', file), done);
+        fs.unlink(path.join('./test-files/', file), done);
       }
     });
+  });
+
+  it('test of test', (done) => {
+    const result = 'test';
+
+    expect(result).toEqual('test');
+    done();
   });
 });
