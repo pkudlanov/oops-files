b923524070e326ee3867817686f291f9a5f3e16a
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-26 17:31:35 -0700

    setup rename files test with before and after commands

--- rename-files.test.js
+++ rename-files.test.js
@@ -1,7 +1,18 @@
-// const { createFiles } = require('./create-files');
+const { createFiles } = require('./create-files');
+const { deleteFiles } = require('./delete-files');
 // const { readDirector, renameFiles } = require('./rename-files');
 
+const directory = './test-files/';
+
 describe('testing rename function', () => {
+    beforeEach(done => {
+        createFiles(directory, 100, done);
+    });
+
+    afterEach(done => {
+        deleteFiles(directory, done);
+    });
+    
     it('passing test', () => {
         const result = 'yes sir';
         expect(result).toEqual('yes sir');
