ea1f83c42145c2f598af855c6bbe4144c9546b82
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-26 17:30:53 -0700

    just some tabbing

--- rename-files.js
+++ rename-files.js
@@ -2,20 +2,22 @@
 
 // const fileFolder = './test-files/';
 
-// const readDirectory = (directory, callback) => {
-//   fs.readdir(directory, (err, files) => {
-//     callback(err, files);
-//   });
-// };
+// function readDirectory(directory, callback) {
+//     fs.readdir(directory, (err, files) => {
+//         callback(err, files);
+//     });
+// }
 
 // function fileRename(oldPath, newName, callback) {
 
-//   fs.readdir('./test-files', { encoding: 'utf8' }, (err, files) => {
-//     for(const file of files) {
-//       fs.rename(`${fileFolder}${file}`, `${fileFolder}/some_other_file.txt`, err => {
-//         if(err) console.error(err);
-//         console.log(`${fileFolder}${files[20]}`);
-//       });
-//     }
-//   });
+//     fs.readdir('./test-files', { encoding: 'utf8' }, (err, files) => {
+//         for(const file of files) {
+//             fs.rename(`${fileFolder}${file}`, `${fileFolder}/some_other_file.txt`, err => {
+//                 if(err) console.error(err);
+//                 console.log(`${fileFolder}${files[20]}`);
+//             });
+//         }
+//     });
 // }
+
+// module.exports = { readDirectory, fileRename }
