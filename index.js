78875a4beaeba2e3104ab8f4ea411c636489b1dd
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-27 10:27:18 -0700

    added the function to the index.js so the user then can run my function in the console

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const { fileRename } = require('./rename-files');
 
+const directory = process.argv[2];
 
+fileRename(directory, err => {
+    if(err) return console.error(err);
+
+    console.log('Your files have been renamed!!');
+});
